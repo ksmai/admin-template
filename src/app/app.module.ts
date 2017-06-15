3c2d3f0d63c4dc238c567b8db3f9c329886e3a07
ksmai <kasingmai@gmail.com>
2017-06-15 15:44:56 +0800

    Implement PanelComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import '../styles/styles.scss';
 import { AppRoutingModule } from './app-routing.module';
--- src/app/shared/panel/panel.component.ts
+++ src/app/shared/panel/panel.component.ts
@@ -1,9 +1,79 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostBinding,
+  Input,
+  OnChanges,
+  ViewChild,
+} from '@angular/core';
 
+/**
+ * PanelComponent
+ * A box for holding DOM elements
+ * Contains optional header and/or footer
+ *
+ * @example
+ * // basic panel
+ * <admin-panel><p>Hello world!</p></admin-panel>
+ *
+ * @example
+ * // panel with header and footer
+ * <admin-panel [showHeader]="true" [showFooter]="true">
+ *   <header>My Header</header>
+ *   <div>some content</div>
+ *   <p>some paragraphs</p>
+ *   <footer>My Footer</footer>
+ * </admin-panel>
+ *
+ * @example
+ * // changing color of header/footer (defaults to blue)
+ * // pass in any valid css colors
+ * <admin-panel [showHeader]="true" headerColor="#666">
+ *   <header>My Header</header>
+ * </admin-panel>
+ * <admin-panel [showFooter]="true" footerColor="rgb(12, 34, 56)">
+ *   <footer>My Footer</footer>
+ * </admin-panel>
+ */
 @Component({
   selector: 'admin-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss'],
 })
-export class PanelComponent {
+export class PanelComponent implements OnChanges, AfterViewInit {
+  @Input() showHeader = false;
+  @Input() showFooter = false;
+  @Input() headerColor = '#79b0ec';
+  @Input() footerColor = '#79b0ec';
+  @ViewChild('header') headerEl: any;
+  @ViewChild('footer') footerEl: any;
+
+  @HostBinding('class.minimized') private isMinimized = false;
+  @HostBinding('class.closed') private isClosed = false;
+
+  ngOnChanges(): void {
+    this.updateColors();
+  }
+
+  ngAfterViewInit(): void {
+    this.updateColors();
+  }
+
+  toggleMinimized(): void {
+    this.isMinimized = !this.isMinimized;
+  }
+
+  toggleClosed(): void {
+    this.isClosed = !this.isClosed;
+  }
+
+  private updateColors(): void {
+    if (this.headerEl) {
+      this.headerEl.nativeElement.style.backgroundColor = this.headerColor;
+    }
+
+    if (this.footerEl) {
+      this.footerEl.nativeElement.style.backgroundColor = this.footerColor;
+    }
+  }
 }
