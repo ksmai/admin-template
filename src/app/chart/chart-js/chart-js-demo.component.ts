405ff62ac335f211dec6e7cbd877edc5e8b56f41
ksmai <kasingmai@gmail.com>
2017-06-15 20:36:53 +0800

    Implement Chart.js charts

--- src/app/chart/chart-js/chart-js-demo.component.ts
+++ src/app/chart/chart-js/chart-js-demo.component.ts
@@ -5,4 +5,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./chart-js-demo.component.scss'],
 })
 export class ChartJSDemoComponent {
+  charts = [
+    {
+      type: 'bar',
+      labels: ['Apple', 'Orange', 'Pineapple', 'Grape', 'Banana'],
+      datasets: {
+        label: '# of fruits',
+        data: [3, 5, 9, 1, 2],
+      },
+    },
+    {
+      type: 'horizontalBar',
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          label: 'Angular',
+          data: [10, 9, 8, 7, 8, 6],
+        },
+        {
+          label: 'React',
+          data: [3, 3, 6, 4, 5, 5],
+        },
+      ],
+    },
+    {
+      type: 'line',
+      labels: ['2011', '2012', '2013', '2014', '2015', '2016'],
+      datasets: [
+        {
+          label: 'vim users',
+          data: [10, 9, 8, 7, 8, 6],
+        },
+        {
+          label: 'emacs user',
+          data: [3, 3, 6, 4, 5, 5],
+        },
+      ],
+    },
+    {
+      type: 'radar',
+      labels: ['Speed', 'Power', 'Height', 'Weight', 'Life expectancy'],
+      datasets: [
+        {
+          label: 'Lion',
+          data: [5, 5, 2, 3, 3],
+        },
+        {
+          label: 'Red Panda',
+          data: [1, 2, 3, 4, 5],
+        },
+      ],
+    },
+    {
+      type: 'pie',
+      labels: ['A', 'B', 'C'],
+      datasets: {
+        data: [1, 2, 3],
+      },
+    },
+    {
+      type: 'doughnut',
+      labels: ['X', 'Y', 'Z'],
+      datasets: [{
+        data: [3, 4, 3],
+      }],
+    },
+    {
+      type: 'polarArea',
+      labels: ['P', 'Q', 'R', 'S', 'T'],
+      datasets: {
+        data: [10, 13, 25, 17, 20],
+      },
+    },
+    {
+      type: 'bubble',
+      datasets: {
+        label: 'My data',
+        data: [
+          { x: 3, y: 1, r: 10 },
+          { x: 2, y: 2, r: 30 },
+          { x: 1, y: 3, r: 20 },
+        ],
+      },
+    },
+  ];
 }
--- src/app/shared/chart/chart.component.ts
+++ src/app/shared/chart/chart.component.ts
@@ -1,9 +1,141 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  OnChanges,
+  AfterViewInit,
+  Input,
+} from '@angular/core';
+import * as Chart from 'chart.js';
+import randomcolor = require('randomcolor');
+
+type Dataset = {
+  label: string;
+  data: Array<number|{x: number, y: number, r?: number }>;
+  backgroundColor?: string|string[];
+  borderColor?: string|string[];
+  pointBackgroundColor?: string|string[];
+  borderWidth?: number;
+};
 
 @Component({
   selector: 'admin-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss'],
 })
-export class ChartComponent {
+export class ChartComponent implements OnChanges, AfterViewInit {
+  @Input() width = 400;
+  @Input() height = 400;
+  @Input() lib: string;
+  @Input() type: string;
+  @Input() labels: string[];
+  @Input() datasets: Dataset|Dataset[];
+  @ViewChild('canvas') canvasEl: any;
+
+  private chart: Chart;
+  private chartType: string;
+
+  ngOnChanges(): void {
+    this.drawChart();
+  }
+
+  ngAfterViewInit(): void {
+    this.drawChart();
+  }
+
+  private drawChart(): void {
+    if (!this.canvasEl) {
+      return;
+    }
+
+    switch (this.lib) {
+      case 'chart-js':
+        this.drawChartJS();
+        break;
+
+      case 'c3-js':
+        this.drawC3JS();
+        break;
+
+      case 'morris-js':
+        this.drawMorrisJS();
+        break;
+
+      default:
+        throw new Error(`Unsupported chart library: ${this.lib}`);
+    }
+  }
+
+  private drawChartJS(): void {
+    if (!this.chart || this.chartType !== 'chart-js') {
+      this.chartType = 'chart-js';
+      const ctx = this.canvasEl.nativeElement;
+      ctx.width = this.width;
+      ctx.height = this.height;
+      this.processChartJSDatasets();
+      const data = {
+        labels: this.labels,
+        datasets: this.datasets,
+      };
+      const options = {
+        legend: {
+          display: (data.datasets as Dataset[]).length > 1,
+        }
+      };
+      this.chart = new Chart(ctx, {
+        data,
+        options,
+        type: this.type,
+      });
+    } else {
+      this.processChartJSDatasets();
+      (this.chart.data as any).datasets = this.datasets;
+      this.chart.update();
+    }
+  }
+
+  private processChartJSDatasets(): void {
+    if (Array.isArray(this.datasets) && this.datasets.length > 1) {
+        const colors = this.generateColors(this.datasets.length);
+        this.datasets.forEach((dataset, idx) => {
+          dataset.backgroundColor = colors.transparent[idx];
+          dataset.borderColor = colors.opaque[idx];
+          dataset.pointBackgroundColor = colors.opaque[idx];
+          dataset.borderWidth = 1;
+        });
+    } else {
+      if (!Array.isArray(this.datasets)) {
+        this.datasets = [this.datasets];
+      }
+      const colors = this.generateColors(this.datasets[0].data.length);
+      this.datasets[0].backgroundColor = colors.transparent;
+      this.datasets[0].borderColor = colors.opaque;
+      this.datasets[0].pointBackgroundColor = colors.opaque;
+      this.datasets[0].borderWidth = 1;
+    }
+  }
+
+  private drawC3JS(): void {
+  }
+
+  private drawMorrisJS(): void {
+  }
+
+  private generateColors(
+    count: number,
+  ): { transparent: string[], opaque: string[] } {
+    const rgbArray = randomcolor({
+      count,
+      format: 'rgb',
+      luminosity: 'bright',
+    }) as any as string[];
+
+    return {
+      transparent: rgbArray.map((rgb) => this.rgb2rgba(rgb, 0.2)),
+      opaque: rgbArray.map((rgb) => this.rgb2rgba(rgb, 1)),
+    };
+  }
+
+  private rgb2rgba(rgb: string, alpha: number) {
+    return rgb.replace(/\)$/, `, ${alpha})`).replace(/^rgb/, 'rgba');
+  }
 }
