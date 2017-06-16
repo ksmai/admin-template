e0aea5a37c6db8fe74232e8fc92883da76395266
ksmai <kasingmai@gmail.com>
2017-06-16 20:40:06 +0800

    Implement basic static timeline

--- src/app/shared/static-timeline/static-timeline.component.ts
+++ src/app/shared/static-timeline/static-timeline.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import randomColor = require('randomcolor');
+
+interface IEvent {
+  date: string;
+  time?: string;
+  texts: string[];
+}
+
+type Luminosity = 'bright'|'dark'|'light'|'random';
 
 @Component({
   selector: 'admin-static-timeline',
   templateUrl: './static-timeline.component.html',
   styleUrls: ['./static-timeline.component.scss'],
 })
-export class StaticTimelineComponent {
+export class StaticTimelineComponent implements OnInit {
+  @Input() events: IEvent[];
+  @Input() luminosity: Luminosity = 'bright';
+  @Input() block: boolean;
+  colors: string[];
+
+  ngOnInit(): void {
+    this.colors = randomColor({
+      count: this.events.length,
+      luminosity: this.luminosity,
+    } as any) as any as string[];
+  }
 }
--- src/app/static-timeline/static-timeline-demo.component.ts
+++ src/app/static-timeline/static-timeline-demo.component.ts
@@ -5,4 +5,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./static-timeline-demo.component.scss'],
 })
 export class StaticTimelineDemoComponent {
+  timelines = [
+    {
+      events: [
+        {
+          date: 'Fri Jun 16 2017',
+          time: '18:01',
+          texts: ['Working on StaticTimelineComponent'],
+        },
+        {
+          date: 'Fri Jun 15 2017',
+          time: '18:01',
+          texts: [
+            'Working on InteractiveTimelineComponent',
+            'Lorem ipsum dolor sit amet',
+          ],
+        },
+      ],
+    },
+    {
+      block: true,
+      events: [
+        {
+          date: 'Fri Jun 16 2017',
+          time: '18:01',
+          texts: ['Working on StaticTimelineComponent'],
+        },
+        {
+          date: 'Fri Jun 15 2017',
+          time: '18:01',
+          texts: [
+            'Working on InteractiveTimelineComponent',
+            'Lorem ipsum dolor sit amet',
+          ],
+        },
+      ],
+    }
+  ];
 }
