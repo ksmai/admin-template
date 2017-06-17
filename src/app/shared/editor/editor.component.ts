ee125804a978b210d3d5a5201d059ad035cdf64c
ksmai <kasingmai@gmail.com>
2017-06-17 17:29:13 +0800

    Implement EditorComponent

--- src/app/shared/editor/editor.component.ts
+++ src/app/shared/editor/editor.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import $ = require('jquery');
+import 'trumbowyg';
+import 'trumbowyg/dist/ui/trumbowyg.min.css';
+
+($ as any).trumbowyg.svgPath = require('trumbowyg/dist/ui/icons.svg');
 
 @Component({
   selector: 'admin-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent {
+export class EditorComponent implements OnInit {
+  constructor(private el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    ($(this.el.nativeElement) as any).trumbowyg({
+      autogrow: true,
+    });
+  }
 }
