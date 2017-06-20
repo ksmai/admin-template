744f37ac8587e786d8b36de3acdc69cdeb8939b0
ksmai <kasingmai@gmail.com>
2017-06-20 14:25:48 +0800

    Implement FontComponent

--- src/app/shared/font/font.component.ts
+++ src/app/shared/font/font.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
+/**
+ * A component wrapper around bootstrap glyphicons
+ * Inputs:
+ *   - name: name of the glyphicon
+ *   - color: optional name of color
+ *
+ * @example
+  <admin-font name="plus" color="blue"></admin-font>
+ */
 @Component({
   selector: 'admin-font',
   templateUrl: './font.component.html',
   styleUrls: ['./font.component.scss'],
 })
-export class FontComponent {
+export class FontComponent implements OnChanges {
+  @Input() name: string;
+  @Input() color: string;
+  classes: { [key: string]: boolean };
+
+  ngOnChanges(): void {
+    this.classes = {
+      glyphicon: true,
+    };
+    this.classes[`glyphicon-${this.name}`] = true;
+    if (this.color) {
+      this.classes[`color-${this.color}`] = true;
+    }
+  }
 }
