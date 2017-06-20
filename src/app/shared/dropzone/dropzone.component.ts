a1edf5e1a2956ec7fe3017e2a344c8f7b7312755
ksmai <kasingmai@gmail.com>
2017-06-20 22:49:28 +0800

    Implement DropzoneComponent

--- src/app/shared/dropzone/dropzone.component.ts
+++ src/app/shared/dropzone/dropzone.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import 'dropzone/dist/basic.css';
+import 'dropzone/dist/dropzone';
+import 'dropzone/dist/dropzone.css';
 
+/**
+ * A component wrapping around Dropzone.js
+ * @example
+ * <admin-dropzone url="/upload"></admin-dropzone>
+ */
 @Component({
   selector: 'admin-dropzone',
   templateUrl: './dropzone.component.html',
   styleUrls: ['./dropzone.component.scss'],
 })
 export class DropzoneComponent {
+  @Input() url: string = '#';
 }
