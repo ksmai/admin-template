4ed792a472ad1d86e91777ec809c17b5d060a917
ksmai <kasingmai@gmail.com>
2017-06-20 19:25:36 +0800

    Implement GridComponent

--- src/app/shared/grid/grid.component.ts
+++ src/app/shared/grid/grid.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  Input,
+  QueryList,
+  TemplateRef,
+} from '@angular/core';
 
+/**
+ * A simple component wrapping around bootstrap grid system
+ * @example
+ * <admin-grid [cols]="[4, 4, 4]">
+ *   <ng-template>Four</ng-template>
+ *   <ng-template>Four</ng-template>
+ *   <ng-template>Four</ng-template>
+ * </admin-grid>
+ */
 @Component({
   selector: 'admin-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss'],
 })
 export class GridComponent {
+  @ContentChildren(TemplateRef) templates: QueryList<TemplateRef<any>>;
+  @Input() cols: string|number[] = [];
 }
