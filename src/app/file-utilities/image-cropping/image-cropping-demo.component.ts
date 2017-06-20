5666b56bb075e70a3866345492e3d808305cd19b
ksmai <kasingmai@gmail.com>
2017-06-20 22:49:39 +0800

    Implement basic image cropping

--- src/app/file-utilities/image-cropping/image-cropping-demo.component.ts
+++ src/app/file-utilities/image-cropping/image-cropping-demo.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   templateUrl: './image-cropping-demo.component.html',
   styleUrls: ['./image-cropping-demo.component.scss'],
 })
 export class ImageCroppingDemoComponent {
+  desk = require('../../../../assets/demo/desk.jpeg');
+  grass = require('../../../../assets/demo/grass.jpeg');
+  bird = require('../../../../assets/demo/bird.jpeg');
+  grassCoord: any;
+  deskSelection = [30, 30, 200, 200];
+  grassSelection = [100, 100, 300, 300];
+  @ViewChild('preview') previewEl: any;
+
+  onGrassChange(c: any): void {
+    this.grassCoord = c;
+  }
+
+  onBirdChange(c: any): void {
+    this.previewEl.nativeElement.style.background =
+      `url(${this.bird})`;
+    this.previewEl.nativeElement.style.backgroundSize =
+      `${200 * c.displayWidth / c.w}px ${200 * c.displayHeight / c.h}px`;
+    this.previewEl.nativeElement.style.backgroundPosition =
+      `left ${c.x * 100 / c.displayWidth}% top ${c.y * 100 / c.displayHeight}%`;
+  }
 }
--- src/app/shared/image-crop/image-crop.component.ts
+++ src/app/shared/image-crop/image-crop.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  ViewChild,
+} from '@angular/core';
+import $ = require('jquery');
+import 'jquery-jcrop/js/jquery.Jcrop.js';
+import 'jquery-jcrop/css/jquery.Jcrop.css';
 
 @Component({
   selector: 'admin-image-crop',
   templateUrl: './image-crop.component.html',
   styleUrls: ['./image-crop.component.scss'],
 })
-export class ImageCropComponent {
+export class ImageCropComponent implements AfterViewInit {
+  @Input() src: string;
+  @Input() aspectRatio: number;
+  @Input() setSelect: [number, number, number, number];
+  @Input() bgColor: string = 'black';
+  @Input() bgOpacity: number = 0.6;
+  @Output() change = new EventEmitter<any>();
+  @ViewChild('img') private imgEl: any;
+
+  ngAfterViewInit(): void {
+    const options = {
+      onChange: (c: any) => this.onChange(c),
+      onSelect: (c: any) => this.onChange(c),
+      bgColor: this.bgColor,
+      bgOpacity: this.bgOpacity,
+    };
+    if (this.aspectRatio) {
+      Object.assign(options, { aspectRatio: this.aspectRatio });
+    }
+    if (this.setSelect) {
+      Object.assign(options, { setSelect: this.setSelect });
+    }
+
+    ($(this.imgEl.nativeElement) as any).Jcrop(options);
+  }
+
+  private onChange(c: any): void {
+    c.displayWidth = parseInt(this.imgEl.nativeElement.style.width, 10);
+    c.displayHeight = parseInt(this.imgEl.nativeElement.style.height, 10);
+    c.imageWidth = this.imgEl.nativeElement.width;
+    c.imageHeight = this.imgEl.nativeElement.height;
+    this.change.emit(c);
+  }
 }
