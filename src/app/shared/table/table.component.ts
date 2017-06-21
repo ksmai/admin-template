b1fb6035b5ae1ab810897c5d724eb122183d7762
ksmai <kasingmai@gmail.com>
2017-06-21 16:56:13 +0800

    Implement Bootstrap Tables

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'admin-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
+  @Input() striped: boolean = false;
+  @Input() bordered: boolean = false;
+  @Input() hover: boolean = false;
+  @Input() condensed: boolean = false;
+  @Input() responsive = false;
+  classes: { [key: string]: boolean };
+
+  ngOnInit(): void {
+    this.classes = {
+      'table': true,
+      'table-striped': this.striped,
+      'table-bordered': this.bordered,
+      'table-hover': this.hover,
+      'table-condensed': this.condensed,
+    };
+  }
 }
