2b443b3d832be498168b02fc137ee2158613e256
ksmai <kasingmai@gmail.com>
2017-06-21 15:21:51 +0800

    Implement advanced features in image crop

--- src/app/file-utilities/image-cropping/image-cropping-demo.component.ts
+++ src/app/file-utilities/image-cropping/image-cropping-demo.component.ts
@@ -11,6 +11,9 @@ export class ImageCroppingDemoComponent {
   grassCoord: any;
   deskSelection = [30, 30, 200, 200];
   grassSelection = [100, 100, 300, 300];
+  animateSelection = [100, 200, 300, 400];
+  bgColor = 'black';
+  bgOpacity = 0.6;
   @ViewChild('preview') previewEl: any;
 
   onGrassChange(c: any): void {
@@ -18,11 +21,47 @@ export class ImageCroppingDemoComponent {
   }
 
   onBirdChange(c: any): void {
-    this.previewEl.nativeElement.style.background =
+    this.previewEl.nativeElement.style.backgroundImage =
       `url(${this.bird})`;
     this.previewEl.nativeElement.style.backgroundSize =
       `${200 * c.displayWidth / c.w}px ${200 * c.displayHeight / c.h}px`;
     this.previewEl.nativeElement.style.backgroundPosition =
-      `left ${c.x * 100 / c.displayWidth}% top ${c.y * 100 / c.displayHeight}%`;
+      `${-c.x * 200 / c.w}px ${-c.y * 200 / c.h}px`;
+  }
+
+  changeAnimateSelection(i: number): void {
+    switch (i) {
+      case 1:
+        this.animateSelection = [100, 10, 200, 200];
+        break;
+
+      case 2:
+        this.animateSelection = [10, 10, 150, 200];
+        break;
+
+      case 3:
+        this.animateSelection = [100, 100, 200, 250];
+        break;
+
+      case 4:
+        this.animateSelection = [200, 100, 250, 300];
+        break;
+
+      case 5:
+        this.animateSelection = [300, 300, 350, 320];
+        break;
+
+      case 0:
+      default:
+        this.animateSelection = [0, 0, 0, 0];
+    }
+  }
+
+  changeOpacity(opacity: number) {
+    this.bgOpacity = opacity;
+  }
+
+  changeColor(color: string) {
+    this.bgColor = color;
   }
 }
--- src/app/shared/image-crop/image-crop.component.ts
+++ src/app/shared/image-crop/image-crop.component.ts
@@ -1,28 +1,62 @@
 import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
   AfterViewInit,
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
   ViewChild,
 } from '@angular/core';
 import $ = require('jquery');
-import 'jquery-jcrop/js/jquery.Jcrop.js';
 import 'jquery-jcrop/css/jquery.Jcrop.css';
+import 'jquery-jcrop/js/jquery.Jcrop.js';
 
+/**
+ * A component wrapping around jquery-jcrop
+ * Inputs:
+ *   - src: source url of the image
+ *   - aspectRatio: optional number, fixed aspect ratio of selection area
+ *   - setSelect: optional [x,y,x2,y2], programmatically set selection
+ *   - bgColor: background color of selection, default to 'black'
+ *   - bgOpacity: opacity of bgColor, default to 0.6
+ *   - bgFade: enable animation of bgColor/bgOpacity, default to true
+ *
+ * Output:
+ *   - change: emits an object after each selection with properties:
+ *     - w: width of selection area
+ *     - h: height of selection area
+ *     - x: offset of the left of selection area to the left edge
+ *     - y: offset of the top of selection area to the top edge
+ *     - x2: offset of the right of selection area to the left edge
+ *     - y2: offset of the bottom of selection area to the top edge
+ *     - displayWidth: width of the image displayed in the page (scaled)
+ *     - displayHeight: height of the image displayed in the page (scaled)
+ *
+ * @example
+ * <admin-image-crop
+ *  [src]="src"
+ *  [setSelect]="selection"
+ *  [bgColor]="bgColor"
+ *  [bgOpacity]="bgOpacity"
+ * ></admin-image-crop>
+ */
 @Component({
   selector: 'admin-image-crop',
   templateUrl: './image-crop.component.html',
   styleUrls: ['./image-crop.component.scss'],
 })
-export class ImageCropComponent implements AfterViewInit {
+export class ImageCropComponent implements AfterViewInit, OnChanges {
   @Input() src: string;
   @Input() aspectRatio: number;
   @Input() setSelect: [number, number, number, number];
   @Input() bgColor: string = 'black';
   @Input() bgOpacity: number = 0.6;
+  @HostBinding('class.fade') @Input() bgFade: boolean = true;
   @Output() change = new EventEmitter<any>();
   @ViewChild('img') private imgEl: any;
+  private jCrop: any;
 
   ngAfterViewInit(): void {
     const options = {
@@ -38,14 +72,37 @@ export class ImageCropComponent implements AfterViewInit {
       Object.assign(options, { setSelect: this.setSelect });
     }
 
-    ($(this.imgEl.nativeElement) as any).Jcrop(options);
+    const self = this;
+    ($(this.imgEl.nativeElement) as any).Jcrop(options, function() {
+      self.jCrop = this;
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!this.jCrop) {
+      return;
+    }
+
+    if (changes.setSelect) {
+      const setSelect = changes.setSelect.currentValue;
+      this.jCrop.animateTo(setSelect);
+      if (setSelect[0] === setSelect[2] || setSelect[1] === setSelect[3]) {
+        setTimeout(() => this.jCrop.release(), 500);
+      }
+    } else {
+      const options = {};
+      for (const key in changes) {
+        if (changes.hasOwnProperty(key)) {
+          options[key] = changes[key].currentValue;
+        }
+      }
+      ($(this.imgEl.nativeElement) as any).Jcrop(options);
+    }
   }
 
   private onChange(c: any): void {
     c.displayWidth = parseInt(this.imgEl.nativeElement.style.width, 10);
     c.displayHeight = parseInt(this.imgEl.nativeElement.style.height, 10);
-    c.imageWidth = this.imgEl.nativeElement.width;
-    c.imageHeight = this.imgEl.nativeElement.height;
     this.change.emit(c);
   }
 }
