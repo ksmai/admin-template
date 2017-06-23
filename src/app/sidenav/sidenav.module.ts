d5859bbf5f69b3eb9f5cb64ad007c5da91d2d5b7
ksmai <kasingmai@gmail.com>
2017-06-23 20:49:54 +0800

    Implement a temporary side nav

--- src/app/sidenav/sidenav.module.ts
+++ src/app/sidenav/sidenav.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { SidenavComponent } from './sidenav.component';
 
 @NgModule({
   imports: [
+    RouterModule,
     SharedModule,
   ],
 
