1aacb698638630a80cbef5f7462ab9a3e5f4b82c
ksmai <kasingmai@gmail.com>
2017-06-24 15:48:18 +0800

    Style demo of EditorComponent

--- src/app/editor/editor-demo.component.ts
+++ src/app/editor/editor-demo.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/**
+ * Demonstate the usage of {@link EditorComponent}
+ */
 @Component({
   templateUrl: './editor-demo.component.html',
   styleUrls: ['./editor-demo.component.scss'],
