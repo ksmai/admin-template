1f5b58ed673e6dd0a2ee0137bdd758f22d11a414
ksmai <kasingmai@gmail.com>
2017-06-25 17:40:33 +0800

    Fix WizardDemoComponent

--- src/app/form/wizard/wizard-demo.component.ts
+++ src/app/form/wizard/wizard-demo.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/**
+ * Demonstrate the usage of {@link FormWizardComponent}
+ */
 @Component({
   templateUrl: './wizard-demo.component.html',
   styleUrls: ['./wizard-demo.component.scss'],
