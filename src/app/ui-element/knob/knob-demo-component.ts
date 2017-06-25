1129fd26d76667a03b6fa6f176c0f09aaba7f97d
ksmai <kasingmai@gmail.com>
2017-06-25 13:31:04 +0800

    Implement different knobs in KnobDemoComponent

--- src/app/ui-element/knob/knob-demo-component.ts
+++ src/app/ui-element/knob/knob-demo-component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
+/**
+ * Demonstrate the usage of KnobComponent
+ */
 @Component({
   templateUrl: './knob-demo-component.html',
   styleUrls: ['./knob-demo-component.scss'],
 })
-export class KnobDemoComponent {
+export class KnobDemoComponent implements OnInit, OnDestroy {
+  hour: number;
+  minute: number;
+  second: number;
+  private intervalID: any;
+
+  ngOnInit(): void {
+    this.getCurrentTime();
+    this.intervalID = setInterval(() => this.getCurrentTime(), 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalID);
+  }
+
+  private getCurrentTime(): void {
+    const now = new Date();
+    this.hour = now.getHours();
+    this.minute = now.getMinutes();
+    this.second = now.getSeconds();
+  }
 }
