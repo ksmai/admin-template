33cfb0b21f23150386784e01eb8e3f2a4c39a604
ksmai <kasingmai@gmail.com>
2017-08-03 20:33:30 +0800

    Implement login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+/**
+ * Render the login page
+ */
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private router: Router) {
+  }
+
+  onLogin(): void {
+    this.router.navigate(['/']);
+  }
 }
