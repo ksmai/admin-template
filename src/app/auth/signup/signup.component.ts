31fc780466d2e1453a5bf35faab769959926ce17
ksmai <kasingmai@gmail.com>
2017-08-03 21:45:38 +0800

    Implement signup page

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+/**
+ * Render the signup page
+ */
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  constructor(private router: Router) {
+  }
+
+  onSignup(): void {
+    this.router.navigate(['/']);
+  }
 }
