4383a5e9375d74da2d680efb14b2144e174f2558
ksmai <kasingmai@gmail.com>
2017-08-03 18:14:25 +0800

    Implement lock page

--- src/app/auth/lock/lock.component.ts
+++ src/app/auth/lock/lock.component.ts
@@ -1,8 +1,59 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
+/**
+ * Display a lock page where user can get back into
+ * the app by providing the password
+ */
 @Component({
   templateUrl: './lock.component.html',
   styleUrls: ['./lock.component.scss'],
 })
-export class LockComponent {
+export class LockComponent implements OnInit, OnDestroy {
+  intervalID: any;
+  hh: string;
+  mm: string;
+  ss: string;
+  day: string;
+  month: string;
+  year: string;
+
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+    'Friday', 'Saturday'];
+  months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
+
+  constructor(private locationService: Location) {
+  }
+
+  ngOnInit() {
+    this.updateTime();
+    this.intervalID = setInterval(() => this.updateTime(), 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalID);
+  }
+
+  onUnlock(): void {
+    this.locationService.back();
+  }
+
+  private pad(s: string, digits = 2): string {
+    let padded = s;
+    while (padded.length < digits) {
+      padded = `0${padded}`;
+    }
+    return padded;
+  }
+
+  private updateTime(): void {
+    const date = new Date();
+    this.hh = this.pad(date.getHours().toString());
+    this.mm = this.pad(date.getMinutes().toString());
+    this.ss = this.pad(date.getSeconds().toString());
+    this.month = this.months[date.getMonth()];
+    this.day = this.days[date.getDay()];
+    this.year = date.getFullYear().toString();
+  }
 }
