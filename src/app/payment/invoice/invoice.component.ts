7d12dd6ca54caf6dc10aac0416daaa566f7bd35e
ksmai <kasingmai@gmail.com>
2017-08-03 17:21:02 +0800

    Implement the invoice page

--- src/app/payment/invoice/invoice.component.ts
+++ src/app/payment/invoice/invoice.component.ts
@@ -1,8 +1,50 @@
 import { Component } from '@angular/core';
+import 'rxjs/add/observable/timer';
+import { Observable } from 'rxjs/Observable';
 
+import { MessengerService } from '../../core/messenger.service';
+
+export interface IInvoiceItem {
+  description: string;
+  quantity: number;
+  unit: string;
+  price: number;
+}
+
+/**
+ * Display an invoice
+ */
 @Component({
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss'],
 })
 export class InvoiceComponent {
+  items: IInvoiceItem[] = [{
+    description: 'Upgrade system to v16.0.7',
+    quantity: 5,
+    unit: 'hours',
+    price: 100,
+  }, {
+    description: 'Deploy website',
+    quantity: 3,
+    unit: 'hours',
+    price: 120,
+  }];
+
+  constructor(private messengerService: MessengerService) {
+  }
+
+  get total(): number {
+    return this.items.reduce((sum, item) =>
+      sum + item.quantity * item.price, 0);
+  }
+
+  onPrint(): void {
+    this.messengerService.notification(
+      'Printing...',
+      'Sorry, not enough credits for this operation',
+      Observable.timer(1000),
+      'invoice-printer',
+    );
+  }
 }
