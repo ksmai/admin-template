72d66fbcc43a941871704e03b3a585fe978bb9db
ksmai <kasingmai@gmail.com>
2017-08-06 17:54:18 +0800

    Implement DashboardMapComponent

--- src/app/dashboard/map/map.component.ts
+++ src/app/dashboard/map/map.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./map.component.scss'],
 })
 export class DashboardMapComponent {
+  lat = 37.7827339;
+  lng = -122.5113068;
+  polyline = {
+    path: [
+      [37.779884, -122.508400],
+      [37.779859, -122.509461],
+      [37.781996, -122.510737],
+      [37.7826649, -122.5112919],
+    ],
+  };
 }
