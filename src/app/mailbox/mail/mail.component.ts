bb18f821d957fa956c28a02c22b67536cedb6ac7
ksmai <kasingmai@gmail.com>
2017-08-06 22:41:33 +0800

    Implement MailComponent inside mailbox

--- src/app/mailbox/mail/mail.component.ts
+++ src/app/mailbox/mail/mail.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/**
+ * Renders a mail in the mailbox
+ */
 @Component({
   templateUrl: './mail.component.html',
   styleUrls: ['./mail.component.scss'],
