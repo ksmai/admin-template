66c12c9dbdd941eeef06ae6cbc6e81821306136c
ksmai <kasingmai@gmail.com>
2017-08-11 15:04:20 +0800

    Implement friend list in sidenav

--- src/app/sidenav/friends/sidenav-friends.component.ts
+++ src/app/sidenav/friends/sidenav-friends.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+const cat = require('../../../../assets/demo/Nyan_cat_250px_frame.jpg');
+
+export interface IFriend {
+  avatar: string;
+  name: string;
+}
+
 /**
  * Renders the friends tab inside sidenav
  */
@@ -9,4 +16,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenav-friends.component.scss'],
 })
 export class SidenavFriendsComponent {
+  onlineFriends = [{
+    avatar: cat,
+    name: 'Nyan Cat 1',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 2',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 3',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 4',
+  }];
+
+  idleFriends = [{
+    avatar: cat,
+    name: 'Nyan Cat 5',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 6',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 7',
+  }];
+
+  busyFriends = [{
+    avatar: cat,
+    name: 'Nyan Cat 8',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 9',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 10',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 11',
+  }];
+
+  offlineFriends = [{
+    avatar: cat,
+    name: 'Nyan Cat 12',
+  }, {
+    avatar: cat,
+    name: 'Nyan Cat 13',
+  }];
 }
