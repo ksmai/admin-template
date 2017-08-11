7653e7cf88258bc6251678cda10744dc87c10f43
ksmai <kasingmai@gmail.com>
2017-08-11 21:11:30 +0800

    Implement timeline inside sidenav

--- src/app/sidenav/timeline/sidenav-timeline.component.ts
+++ src/app/sidenav/timeline/sidenav-timeline.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import {
+  IEvent,
+} from '../../shared/static-timeline/static-timeline.component';
+
 /**
  * Render the timeline tab inside sidenav
  */
@@ -9,4 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenav-timeline.component.scss'],
 })
 export class SidenavTimelineComponent {
+  events: IEvent[] = [{
+    date: '11 Aug 2017',
+    templates: [0],
+    hasAvatar: [true],
+  }, {
+    date: '10 Aug 2017',
+    templates: [1, 2],
+    hasAvatar: [true, true],
+  }, {
+    date: '9 Aug 2017',
+    templates: [3],
+    hasAvatar: [true],
+  }];
 }
